cc173d29d9ee20ef546c5983bb2fc1656d490489
Aimee <aimeehorwood@hotmail.com>
2022-11-08 15:19:50 +0000

    Adds codeword_spec.rb

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,7 +1,27 @@
 require "check_codeword"
-
 RSpec.describe "check_codeword method" do
-  it "returns the wrong message if given the wrong codeword" do 
+  it "returns the wrong message if given the wrong codeword" do
     result = check_codeword("round")
-    expect(result).to eq "WRONG"
-  end 
+    expect(result).to eq "WRONG!"
+  end
+
+  it "returns a close message if given a word that is close" do
+    result = check_codeword("hose")
+    expect(result).to eq "Close, but nope."
+  end
+
+  it "returns a correct message if its given the right codeword" do
+    result = check_codeword("horse")
+    expect(result).to eq "Correct! Come in."
+  end
+
+  it "returns a wrong message if the codeword has only the correct first letter" do
+    result = check_codeword("hat")
+    expect(result).to eq "WRONG!"
+  end
+
+  it "returns a wrong message if the codeword has only the correct last letter" do
+    result = check_codeword("toe")
+    expect(result).to eq "WRONG!"
+  end
+end
